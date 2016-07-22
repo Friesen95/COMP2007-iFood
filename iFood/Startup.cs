99568620a82d7ea1ded05b6ea79cdf4f38378f3b
Alex Friesen <friesen.alex95@gmail.com>
2016-07-22 14:55:01 -0400

    Init Commit
    Initial commit

---
+++ iFood/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iFood.Startup))]
+namespace iFood
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
