95cd4187091b27d739621b709e6364f213ca0ac2
Alex Friesen <friesen.alex95@gmail.com>
2016-07-27 21:00:28 -0400

    Added another Food Item to the List in Data

--- iFood/Models/SampleData.cs
+++ iFood/Models/SampleData.cs
@@ -23,7 +23,8 @@ namespace iFood.Models
                 new FoodItemModels {Name = "Spanish Tortilla", description="Thinly fried sliced of potatoes  mixed with vegetables, onion and multiple spices soaked in a stirred egg and cut into small cubes", Price=10.25M, FoodType = types.Single(g => g.Name == "Appetizer"), FoodArtUrl = "/Content/Images/small-bites-spanish-tortilla.jpg" },
                 new FoodItemModels {Name = "Fried Pork Dumplings", description = "Pork, Bacon, cabbage and multiple spices wrapped up in a Dumpling form, served with ponzu dipping souce", Price=10.99M, FoodType= types.Single(g => g.Name == "Appetizer"), FoodArtUrl = "/Content/Images/Fried-Pork-Dumplings.jpg" },
                 new FoodItemModels {Name = "Skillet Chicken and Ravioli", description ="Seasoned chicken  mixed with classic ravioli, mushrooms, tomatoes, and parmesan all stir fryed in a skillet with in house seasoning", Price=23.55M, FoodType= types.Single(g => g.Name == "Main Course"), FoodArtUrl="/Content/Images/Skillet-Ravioli.jpg" },
-                new FoodItemModels {Name = "Tuscan Lemon Chicken", description="A Full Chicken house marinated fire grilled served with grilled lemons and your choice of    " }
+                new FoodItemModels {Name = "Tuscan Lemon Chicken", description="A Full Chicken house marinated fire grilled served with grilled lemons and your choice of any house made sides. excluding some of the exclusives marked exl.", Price=27.24M, FoodType= types.Single(g => g.Name == "Main Course"), FoodArtUrl="/Content/Images/Tuscan-Lemon-Chicken.png" }
+
             }.ForEach(a => context.FoodItems.Add(a));
     }
     }
