1bc763ab2bb08d2348e5a764e4e69b6b89307996
SamSunny95 <sampsunny1995@gmail.com>
2016-07-29 22:11:17 -0400

    Added StoremanagerController

--- iFood/Controllers/RestaurantManagerController.cs
+++ iFood/Controllers/RestaurantManagerController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using iFood.Models;
+using System.Data;
+using System.Data.Entity;
 
 namespace iFood.Controllers
 {
@@ -14,8 +16,95 @@ namespace iFood.Controllers
         // GET: RestaurantManager
         public ActionResult Index()
         {
-            //var fooditem = db.FoodItems.Include(a => a.FoodType);
-            return View(/*fooditem.ToList()*/);
+            List<FoodItemModels>fooditem = db.FoodItems.ToList();
+            return View(fooditem.ToList());
+        }
+        //
+        // GET: /StoreManager/Details/5
+
+        public ViewResult Details(int id)
+        {
+            FoodItemModels FoodItem = db.FoodItems.Find(id);
+            return View(FoodItem);
+        }
+
+        //
+        // GET: /StoreManager/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.FoodTypeId = new SelectList(db.FoodTypes, "FoodTypeId", "Name");
+            return View();
+        }
+
+        //
+        // POST: /StoreManager/Create
+
+        [HttpPost]
+        public ActionResult Create(FoodItemModels FoodItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FoodItems.Add(FoodItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FoodTypeId = new SelectList(db.FoodTypes, "FoodTypeId", "Name", FoodItem.FoodTypeId);
+            return View(FoodItem);
+        }
+
+        //
+        // GET: /StoreManager/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            FoodItemModels FoodItem = db.FoodItems.Find(id);
+            ViewBag.FoodTypeId = new SelectList(db.FoodTypes, "FoodTypeId", "Name", FoodItem.FoodTypeId);
+            return View(FoodItem);
+        }
+
+        //
+        // POST: /StoreManager/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(FoodItemModels FoodItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(FoodItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FoodTypeId = new SelectList(db.FoodTypes, "FoodTypeId", "Name", FoodItem.FoodTypeId);
+            return View(FoodItem);
+        }
+
+        //
+        // GET: /StoreManager/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            FoodItemModels FoodItem = db.FoodItems.Find(id);
+            return View(FoodItem);
+        }
+
+        //
+        // POST: /StoreManager/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FoodItemModels FoodItem = db.FoodItems.Find(id);
+            db.FoodItems.Remove(FoodItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
